732b6eab3e9529eed2444fc5eda2d294703ae6f8
Admin <Admin@PC022>
2024-01-09 21:05:29 +0700

    up

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
+    private float movement;
     [SerializeField] private Rigidbody2D rigid;
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private SpriteRenderer playerSprite;
     // Start is called before the first frame update
     private void OnValidate()
     {
         rigid = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        movement = Input.GetAxisRaw("Horizontal") * speed;
     }
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(horizontal * speed, rigid.velocity.y);
+        Vector2 velocity = rigid.velocity;
+        velocity.x = movement;
+        rigid.velocity = velocity;
+        if (movement < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else if (movement > 0)
+        {
+            playerSprite.flipX = false;
+        }
     }
 }
---
+++ Assets/_Scripts/SpawnPlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab;
+
+    [SerializeField] private int numberOfPlatforms = 200;
+    [SerializeField] private float levelWidth = 2f;
+    [SerializeField] private float minY = 0.2f;
+    [SerializeField] private float maxY = 1.5f;
+
+    [SerializeField] private float distancePlatform;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+    private void Update()
+    {
+        
+    }
+}
